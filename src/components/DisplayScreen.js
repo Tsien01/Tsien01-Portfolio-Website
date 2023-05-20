5edc081ea635fd8daf3df0e330f3bd0cbb7826d3
Tsien01 <tsien.jack@gmail.com>
2023-05-20 23:44:52 +0100

    added text scrambling effect to root.js title and displayScreen component, as well as functionality to toggle off text scrambling for displayScreen optionally

--- src/components/DisplayScreen.js
+++ src/components/DisplayScreen.js
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react"
 import "./DisplayScreen.css"
+import { generateRandomLetter } from "../utils";
 
-export default function DisplayScreen ({ displayText, imageURL }) {
+export default function DisplayScreen ({ displayText, imageURL, useTextScramble = true }) {
+    const [ screenTitle, setScreenTitle ] = useState(""); 
 
+    useEffect(() => {
+        setScreenTitle(displayText)
+
+        if (useTextScramble) {
+            const titleText = displayText; 
+            let iterations = 0; 
+            const interval = setInterval(() => {
+                const scrambledTitle = titleText.split("").map((letter, index) => {
+                    if (index < iterations) {
+                        return letter; 
+                    }
+                    return generateRandomLetter(); 
+                })
+    
+                setScreenTitle(scrambledTitle); 
+    
+                if (iterations >= titleText.length) {
+                    clearInterval(interval); 
+                    setScreenTitle(titleText); 
+                }
+    
+                iterations += 1; 
+            }, 100)
+            return (() => {clearInterval(interval)})
+        }
+    }, [])
 
     return (
         <div className="display" 
         style={{ backgroundImage: `url(${imageURL})` }} >
             <div className="screen">
-                <span>{displayText}</span>
+                <span>{useTextScramble ? screenTitle : displayText}</span>
             </div>
         </div>
     )
--- src/routes/root.js
+++ src/routes/root.js
@@ -1,34 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ThreeDisplay from "../components/ThreeDisplay";
 import "./root.css"; 
 import DisplayScreen from "../components/DisplayScreen";
 import { Link } from "react-router-dom";
+import { generateRandomLetter } from "../utils";
 import leftBackgroundImg from "../img/left.jpg"
 import centerBackgroundImg from "../img/center.jpg"
 import rightBackgroundImg from "../img/right.jpg"
 
 
 export default function Root() {
+  //enable 3d state and toggle
   const [threeEnabled, setThreeEnabled] = useState(false); 
 
   function handleOnClick () {
     setThreeEnabled(!threeEnabled); 
   }
-  
+  //text scrambling effect
+  const [title, setTitle] = useState("")
+
+  useEffect(() => {
+    const titleText = "JACK TSIEN";
+    let iterations = 0
+    const interval  = setInterval(() => {
+      //generate scrambled title string where each word resolves one-by-one
+      const scrambledTitle = titleText.split("").map((letter, index) => {
+        if (index < iterations) {
+          return letter; 
+        }
+        return generateRandomLetter(); 
+      })
+      //setState to random characters
+      setTitle(scrambledTitle)
+      //stop after 10 cycles
+      if (iterations >= titleText.length) {
+        clearInterval(interval); 
+        setTitle(titleText);
+      }; 
+      //increment iterations
+      iterations += 1;  
+    }, 100);  
+    return (() => {clearInterval(interval)})
+  }, [])  
 
   return (
       <div id="page">
-        <h1 className="title">Jack Tsien</h1>
         <button className="threeToggleControl" onClick={handleOnClick}>{threeEnabled ? "Disable 3D" : "Enable 3D"}</button>
         <div className="screenContainer">
           <Link to={"/About"} className="link">
-            <DisplayScreen displayText={"About"} imageURL={leftBackgroundImg}></DisplayScreen>
+            <DisplayScreen displayText={"ABOUT"} imageURL={leftBackgroundImg} useTextScramble={true}></DisplayScreen>
           </Link>
           <Link to={"/"} className="link">
-            <DisplayScreen displayText={"Start?"} imageURL={centerBackgroundImg}></DisplayScreen>
+            <h1 className="title">{title}</h1>
+            <DisplayScreen displayText={"START?"} imageURL={centerBackgroundImg} useTextScramble={true}></DisplayScreen>
           </Link>
           <Link to={"/Projects"} className="link">
-            <DisplayScreen displayText={"Projects"} imageURL={rightBackgroundImg}></DisplayScreen>
+            <DisplayScreen displayText={"PROJECTS"} imageURL={rightBackgroundImg} useTextScramble={true}></DisplayScreen>
           </Link>
         </div>
         {threeEnabled ? <ThreeDisplay></ThreeDisplay> : <div />}
